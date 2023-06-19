5cdd7a88897bb166750dc0302c84665602380b7b
Blake Regalia <blake.regalia@gmail.com>
2023-06-19 03:11:56 -0700

    dev: stable

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-	import { hex_to_buffer, type AsJson, type JsonObject, timeout } from '@blake.regalia/belt';
-	import {bech32_decode, exec_contract, SecretContract, type SecretBech32, msgGrantAllowance, anyBasicAllowance, broadcast_result, create_and_sign_tx_direct, sign_query_permit, query_contract_infer} from '@solar-republic/neutrino';
+	import {base64_to_buffer, type AsJson, type JsonObject, sha256, text_to_buffer, buffer_to_base58, buffer_to_base93, buffer_to_base64, type Base64, buffer_to_text} from '@blake.regalia/belt';
+	import {timeout} from '@blake.regalia/belt';
+	import {bech32_decode, exec_contract, SecretContract, type SecretBech32, msgGrantAllowance, anyBasicAllowance, broadcast_result, create_and_sign_tx_direct, sign_query_permit, query_contract_infer, bech32_encode} from '@solar-republic/neutrino';
 
 	import { getContext, setContext, tick } from 'svelte';
 	import WalletSvelte from './Wallet.svelte';
@@ -103,20 +104,66 @@
 			channel: 'message',
 		}, g_permit);
 
-		debugger;
+		if(s_error) {
+			y_wallet.cta('Testnet Instability', [
+				'The API node failed to process a query. This can happen occKionally on the pulsar testnet. Wait a few minutes and try reloading.',
+			], 'Reload', () => {
+				location.reload();
+			});
+		}
+		else {
+			subscribe_snip52_channels(K_WALLET.rpc, k_contract, g_permit, {
+				message(a_data) {
+					const [atu8_sender, s_message] = a_data as [Uint8Array, string];
 
-		console.log(g_result, xc_code, s_error);
+					const sa_sender = bech32_encode('secret', atu8_sender) as SecretBech32;
 
+					h_chats[sa_sender].messages.push({
+						time: Date.now(),
+						text: s_message,
+						reactions: [],
+						me: false,
+					});
 
-		subscribe_snip52_channels(K_WALLET.rpc, k_contract, g_permit, {
-			message() {
+					// invalidate chats object and update storage
+					ls_write_json('history', (h_chats=h_chats) as AsJson<typeof h_chats>);
+					
+					// invalidate convo
+					if(sa_sender === g_convo?.contact.address) {
+						g_convo = h_chats[sa_sender];
+						scroll_bottom();
+					}
+				},
 
-			},
+				async reaction(a_data) {
+					const [atu8_sender, atu8_hash, s_emoji] = a_data as [Uint8Array, Uint8Array, string];
 
-			reaction() {
+					const sa_sender = bech32_encode('secret', atu8_sender) as SecretBech32;
+					const sb64_msg_hash = buffer_to_base64(atu8_hash);
 
-			},
-		});
+					// find message they are reacting to
+					for(let g_msg of h_chats[sa_sender].messages.slice().reverse()) {
+						const sb64_hash = buffer_to_base64(await sha256(text_to_buffer(g_msg.text)));
+						if(sb64_hash === sb64_msg_hash) {
+							// add reaction
+							g_msg.reactions.push(s_emoji);
+
+							// invalidate chats object and update storage
+							ls_write_json('history', h_chats=h_chats as AsJson<typeof h_chats>);
+
+							// invalidate convo
+							if(sa_sender === g_convo?.contact.address) {
+								g_convo = h_chats[sa_sender];
+								scroll_bottom();
+							}
+
+							// done
+							break;
+						}
+					}
+				},
+			});
+		}
 	})();
 
 	async function new_contact() {
@@ -154,6 +201,15 @@
 			contact: g_contact,
 			messages: [],
 		});
+
+		scroll_bottom();
+	}
+
+	let dm_scroll: HTMLDivElement;
+	async function scroll_bottom() {
+		await tick();
+
+		dm_scroll.scrollTo(0, dm_scroll.scrollHeight);
 	}
 
 	let s_chat_message = '';
@@ -174,11 +230,8 @@
 			},
 		}, [['2500', 'uscrt']], '50000', '', K_GRANTER.addr);
 
-		debugger;
-		console.log(xc_code, sx_res, g_tx_res);
-
 		// add message to convo
-		// if(!xc_code && g_tx_res) {
+		if(!xc_code && g_tx_res) {
 			h_chats[sa_recipient].messages.push({
 				time: Date.now(),
 				text: s_chat_message,
@@ -193,11 +246,21 @@
 			if(sa_recipient === g_convo.contact.address) {
 				g_convo = h_chats[sa_recipient];
 			}
-		// }
 
-		// clear input and re-enable it
-		s_chat_message ='';
-		b_chat_disabled = false;
+			// clear input and re-enable it
+			s_chat_message = '';
+			b_chat_disabled = false;
+
+			scroll_bottom();
+		}
+		else {
+			y_wallet.cta('Transaction Failed', [
+				sx_res,
+			], 'Retry', () => {
+				send_message();
+				y_wallet.reset_menu();
+			});
+		}
 
 		void y_wallet.refresh_spendable_gas();
 	}
@@ -229,6 +292,47 @@
 		}
 	}
 
+	let g_msg_reaction_busy: Message | null = null;
+	let s_reaction_pending = '';
+	async function react(g_msg: Message, s_emoji: string) {
+		g_msg_reaction_busy = g_msg;
+		s_reaction_pending = s_emoji;
+
+		// submit tx
+		const [xc_code, sx_res, g_tx_res] = await exec_contract(await dp_contract, K_WALLET, {
+			react: {
+				author: g_convo?.contact.address,
+				message_hash: buffer_to_base64(await sha256(text_to_buffer(g_msg.text))),
+				reaction: s_emoji,
+			},
+		}, [['2500', 'uscrt']], '50000', '', K_GRANTER.addr);
+
+		// add reaction
+		g_msg.reactions.push(s_emoji);
+
+		// invalidate convo
+		g_convo = g_convo;
+
+		// invalidate chats object and update storage
+		ls_write_json('history', h_chats as AsJson<typeof h_chats>);
+
+		g_msg_reaction_busy = null;
+		s_reaction_pending = '';
+
+		scroll_bottom();
+	}
+
+	function block_mouse_enter(this: HTMLDivElement) {
+		if(!g_msg_reaction_busy) {
+			this.classList.add('hovering');
+		}
+	}
+
+	function block_mouse_leave(this: HTMLDivElement) {
+		if(!g_msg_reaction_busy) {
+			this.classList.remove('hovering');
+		}
+	}
 </script>
 
 <style lang="less">
@@ -239,6 +343,14 @@
 
 		margin: 1em 2em;
 		overflow: hidden;
+	}
+
+	a {
+		color: #eea;
+
+		&:visited {
+			color: #eea;
+		}
 	}
 
 	code {
@@ -286,6 +398,7 @@
 	.app {
 		display: flex;
 		width: 100%;
+		overflow: hidden;
 
 		.nav {
 			border-right: 1px solid #888;
@@ -340,18 +453,122 @@
 		.chat {
 			flex-grow: 1;
 			max-width: 800px;
+			height: 100%;
 
 			display: flex;
 			flex-direction: column;
 			gap: 1em;
 
+			align-self: self-end;
+
+			&.align-self_auto {
+				align-self: auto;
+			}
+
+			.scroll {
+				overflow: scroll;
+			}
+
 			.convo {
 				flex-grow: 1;
-				padding-bottom: 1em;
-				border-bottom: 1px solid #888;
+
+				display: flex;
+				flex-direction: column;
+				gap: 0px;
+
+				.intro {
+					max-width: 600px;
+					line-height: 1.5em;
+
+					h2:not(:first-child) {
+						margin-top: 2em;
+					}
+				}
+
+				.block {
+					padding: 6px 0;
+
+					&.hovering>.metadata>.react:not(.never) {
+						visibility: visible;
+					}
+
+					&:not(.me) {
+						.who {
+							color: orange;
+						}
+					}
+
+					.metadata {
+						display: flex;
+						align-items: baseline;
+						gap: 1em;
+
+						.who {
+							font-weight: 600;
+							font-size: 12px;
+						}
+
+						.when {
+							font-size: 11px;
+							color: #889;
+						}
+
+						.react {
+							visibility: hidden;
+							display: flex;
+							gap: 4px;
+
+							&.busy {
+								opacity: 0.6;
+							}
+
+							&:not(.busy) {
+								>span:hover {
+									border-color: #222;
+									background: #222;
+									border-radius: 4px;
+								}
+							}
+
+							>span {
+								padding: 3px 6px;
+								border: 1px solid transparent;
+								cursor: pointer;
+
+								&.locked {
+									border-color: #222;
+									background: #222;
+									border-radius: 4px;
+								}
+							}
+						}
+					}
+
+					.message {
+						padding: 0;
+					}
+
+					.reactions {
+						display: flex;
+						gap: 4px;
+						margin-top: 5px;
+
+						&>span {
+							background: #262629;
+							font-size: 11px;
+							border: 1px solid #111;
+							padding: 2px 5px;
+							border-radius: 4px;
+						}
+					}
+				}
 			}
 
 			.draft {
+				border-top: 1px solid #888;
+				padding-top: 1em;
+				margin-bottom: 1px;
+
 				flex-basis: 50px;
 				display: flex;
 				gap: 2px;
@@ -439,36 +656,97 @@
 
 		<div class="contacts">
 			{#each Object.entries(h_chats) as [sa_contact, g_chat] (sa_contact)}
-				<div class:active={g_chat.contact.address === sa_contact}>
+				<div class:active={g_convo?.contact.address === sa_contact} on:click={() => load_chat(g_chat.contact)}>
 					{g_chat.contact.name ?? sa_contact}
 				</div>
 			{/each}
 		</div>
 	</div>
 
-	<div class="chat">
-		<div class="convo">
-			{#each (g_convo?.messages || []) as g_msg, i_msg (i_msg)}
-				<div class="metadata">
-					<span class="who">
-						{#if g_msg.me}
-							You:
-						{:else}
-							{g_convo?.contact.name ?? g_convo?.contact.address}
-						{/if}
-					</span>
-					<span class="when">
-						{new Date(g_msg.time).toISOString()}
-					</span>
-				</div>
-				<div class="message">
-					{g_msg.text}
-				</div>
-			{/each}
+	<div class="chat" class:align-self_auto={!g_convo}>
+		<div class="scroll" bind:this={dm_scroll}>
+			<div class="convo">
+				{#if !g_convo}
+					<div class="intro">
+						<h2>
+							What is this?
+						</h2>
+
+						<p>
+							This app is a demonstration of <a href="https://github.com/SolarRepublic/SNIPs/blob/feat/snip-52/SNIP-52.md">SNIP-52: Private Push Notifications</a> for Secret Network. It is not meant to be used as an actual chat app.
+						</p>
+
+						<h2>
+							What am I looking at?
+						</h2>
+
+						<p>
+							Your browser generated a unique hot wallet for you when you opened the page. That wallet has already, or is currently being, funded to send transactions on the Pulsar testnet, see wallet in top right corner.
+						</p>
+						
+						<h2>
+							What do I do?
+						</h2>
+
+						<p>
+							Send your address to a friend, or ask for theirs. Paste an address in the input on the left to start a new chat.
+						</p>
+						
+						<h2>
+							What's the point?
+						</h2>
+
+						<p>
+							If you check your network log, you'll notice that the app does not make queries to the contract to discover new incoming messages. Instead, the contract emits notifications which the node "pushes" to your device via WebSocket.
+						</p>
+					</div>
+				{:else}
+					{#each (g_convo?.messages || []) as g_msg, i_msg (i_msg)}
+						<div class="block"
+							class:me={g_msg.me}
+							class:hovering={g_msg === g_msg_reaction_busy}
+							on:mouseenter={block_mouse_enter}
+							on:mouseleave={block_mouse_leave}
+						>
+							<div class="metadata">
+								<span class="who">
+									{#if g_msg.me}
+										You:
+									{:else}
+										{g_convo?.contact.name ?? g_convo?.contact.address}
+									{/if}
+								</span>
+								<span class="when">
+									{new Date(g_msg.time).toLocaleString()}
+								</span>
+								<span class="react" class:never={g_msg.me} class:busy={g_msg === g_msg_reaction_busy}>
+									{#each ['👍', '🤣', '💯', '🔥'] as s_emoji}
+										<span class:locked={s_emoji === s_reaction_pending}
+											on:click={() => react(g_msg, s_emoji)}
+										>
+											{s_emoji}
+										</span>
+									{/each}
+								</span>
+							</div>
+							<div class="message">
+								{g_msg.text}
+							</div>
+							{#if g_msg.reactions.length}
+								<div class="reactions">
+									{#each g_msg.reactions as s_reaction}
+										<span>{s_reaction}</span>
+									{/each}
+								</div>
+							{/if}
+						</div>
+					{/each}
+				{/if}
+			</div>
 		</div>
 		<div class="draft">
-			<textarea bind:this={dm_draft} disabled={b_chat_disabled} bind:value={s_chat_message} on:keydown={keydown}></textarea>
-			<button disabled={b_chat_disabled}>
+			<textarea bind:this={dm_draft} disabled={!g_convo || b_chat_disabled} bind:value={s_chat_message} on:keydown={keydown}></textarea>
+			<button disabled={!g_convo || b_chat_disabled}>
 				Send{b_chat_disabled? 'ing...': ''}
 			</button>
 		</div>
